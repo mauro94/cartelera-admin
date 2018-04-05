1c68c74b538b26844a770e87fb5996ccdd320911
Andres Altamirano <andres.altamirano.m@gmail.com>
2018-04-05 16:01:08 -0500

    remove incorrectly added layout

--- src/app/components/presentational/layout/index.js
+++ src/app/components/presentational/layout/index.js
@@ -1,7 +1,5 @@
 import Admin from './Admin'
 import Sponsor from './Sponsor'
-import Category from './Category'
 
 export const AdminLayout = Admin
 export const SponsorLayout = Sponsor
-export const CategoryLayout = Category
