95490beae69d6a0ace9d7be33d7f0c9230576a88
Mauro Amarante <mauro@Mauros-MacBook-Pro-2.local>
2018-04-08 22:25:32 -0500

    WIP - Events edit form

---
+++ src/app/components/presentational/elements/DatePickerElement.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import DatePicker from 'react-datepicker'
+import moment from 'moment'
+
+import 'react-datepicker/dist/react-datepicker.css'
+
+export class DatePickerElement extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            startDate: moment(),
+            endDate: moment(),
+            placeholder: props.placeholder
+        }
+    }
+    
+    handleChange({ startDate, endDate }) {
+        startDate = startDate || this.state.startDate
+        endDate = endDate || this.state.endDate
+    
+        if (startDate.isAfter(endDate)) {
+          endDate = startDate
+        }
+    
+        this.setState({ startDate, endDate })
+      }
+    
+    handleChangeStart(startDate){
+        this.handleChange({ startDate:startDate })
+    }
+
+    handleChangeEnd(endDate) {
+        this.handleChange({ endDate:endDate })
+    }
+
+    render() {
+        if (this.props.type == "selectsStart")
+            return (
+                <DatePicker
+                    showTimeSelect
+                    timeFormat="HH:mm"
+                    minDate={moment()}
+                    maxDate={moment().add(1, "year")}
+                    dateFormat="LLL"
+                    timeCaption="Tiempo"
+                    placeholderText={this.state.placeholder}
+                    locale="es-mx"
+                    isClearable={true}
+                    selected={this.state.startDate}
+                    selectsStart
+                    startDate={this.state.startDate}
+                    endDate={this.state.endDate}
+                    onChange={this.handleChangeStart}
+                />
+            )
+        else
+        return (
+            <DatePicker
+                showTimeSelect
+                timeFormat="HH:mm"
+                minDate={moment()}
+                maxDate={moment().add(1, "year")}
+                dateFormat="LLL"
+                timeCaption="Tiempo"
+                placeholderText={this.state.placeholder}
+                locale="es-mx"
+                isClearable={true}
+                selected={this.state.endDate}
+                selectsEnd
+                startDate={this.state.startDate}
+                endDate={this.state.endDate}
+                onChange={this.handleChangeEnd}
+            />
+        )
+    }
+}
+
