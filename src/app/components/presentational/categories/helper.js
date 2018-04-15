225c8e2b41829aab524669252c72b11c4ed59421
Andres Altamirano <andres.altamirano.m@gmail.com>
2018-04-15 14:41:34 -0500

    remove unused imports

--- src/app/components/presentational/categories/helper.js
+++
@@ -1,8 +0,0 @@
-import { Entity } from 'Helpers/index'
-
-export const getDefaultCategoryId = (categories) => {
-    if (!Entity.isEmpty(categories)) {
-        return categories[0].id
-    }
-    return -2
-}
