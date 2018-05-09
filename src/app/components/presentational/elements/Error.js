b467a2d3c4a68ce1bb3bcc0d42fe27f3b29b0b5e
Natalia <garcia.doe@gmail.com>
2018-05-08 20:15:00 -0500

    Added component for 404 page not found and server error

--- src/app/components/presentational/elements/Error.js
+++ src/app/components/presentational/elements/Error.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import PageNotFoundIcon from 'Images/pageNotFound.svg'
+import ServerErrorIcon from 'Images/serverError.svg'
+import 'Style/common/emptyIcon.scss'
 
-const Error = ({ message }) => (
-    <h3 className="generic-error">{message}</h3>
+export const ServerError = (props) => (
+    <ErrorElement>
+        <img className='error-icon' src={ServerErrorIcon} />
+        <div>{props.errorMessage}</div>
+    </ErrorElement>
 )
 
-export default Error
+export const PageNotFound = (props) => (
+    <ErrorElement>
+        <img className='error-icon' src={PageNotFoundIcon} />
+        <div>Esta página no existe</div>
+    </ErrorElement>
+)
+
+export const ErrorElement = ({ children }) => (
+    <div className='error-container'>
+        <h3 className='generic-error'>{children}</h3>
+    </div>
+)
